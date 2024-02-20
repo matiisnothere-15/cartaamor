b2d3d74d975af43386909776a0b6eae6d7e1f774
matias <matiasriquelme633@gmail.com>
2024-02-20 12:15:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    var envlopeWrapper = $(".envlope-wrapper");
+  
+    envelope.click(function () {
+      open();
+    });
+  
+    btn_open.click(function () {
+      open();
+    });
+  
+    btn_reset.click(function () {
+      close();
+    });
+  
+    function open() {
+      envelope.addClass("open").removeClass("close");
+      envlopeWrapper.addClass('fade-out');
+      $('#userInput').addClass('visible'); // Muestra el área de texto
+      setDefaultText(); // Establece el texto predeterminado
+      $('.letter').addClass('growing');
+    }
+  
+    function close() {
+      envelope.addClass("close").removeClass("open");
+      envlopeWrapper.removeClass('fade-out');
+      $('#userInput').removeClass('visible'); // Oculta el área de texto
+      $('.letter').removeClass('growing');
+    }
+  
+    function setDefaultText() {
+      // Puedes ajustar el texto predeterminado aquí
+      $('#line1').text('¡Hola amorcito!');
+      $('#line2').text('Solo quería recordarte lo especial que eres para mí. Eres mi luz, mi risa y mi razón para sonreír todos los días. Gracias por hacer mi vida increíble.');
+      $('#line3').text('Cada momento contigo es un regalo precioso que atesoro con cariño. Gracias por ser mi apoyo incondicional y por compartir risas y lágrimas a mi lado . ');
+      $('#line4').text('Con todo mi amor,[Matías]');
+    }
+  });
+  
